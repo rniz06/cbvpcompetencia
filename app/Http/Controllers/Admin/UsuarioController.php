e38be85a121672a1affd82a1ff925687b7fbac9e
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-10-20 14:31:50 -0300

    Subida de Proyecto

---
+++ app/Http/Controllers/Admin/UsuarioController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+        return view('admin.usuarios.index');
+    }
+
+    public function create()
+    {
+        return view('admin.usuarios.create');
+    }
+
+    public function asignarRolAUsuario()
+    {
+        return view('admin.usuarios.asignar-rol-a-usuarios');
+    }
+
+    public function edit($usuario)
+    {
+        return view('admin.usuarios.edit', compact('usuario'));
+    }
+
+    public function asignarRol($usuario)
+    {
+        return view('admin.usuarios.asignar-rol', compact('usuario'));
+    }
+}
